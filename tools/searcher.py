dba973d5343a239a4d19009bcd73542b97c17fa9
Thomas Maier <tmaier89@gmx.net>
2018-05-11 12:22:27 +0200

    add output parsing to searcher

--- tools/searcher.py
+++ tools/searcher.py
@@ -10,5 +10,24 @@ class Searcher:
 
     def search(self):
         result = self._instance.search(index = self._index, body = self._query, size = 1)
+        output = {}
+        for agg_name, agg_data in result['aggregations'].items():
+            Searcher._parse(agg_name, agg_data, output)
         with open(self._output, 'w') as out_file:
-            json.dump(result, out_file)
+            json.dump(output, out_file)
+
+    @staticmethod
+    def _parse(agg_name, agg_data, output):
+        output[agg_name] = {}
+        for entry in agg_data['buckets']:
+            agg_key = entry['key']
+            output[agg_name][agg_key] = {}
+            count = entry['doc_count']
+            output[agg_name][agg_key]['count'] = count
+            if len(entry) > 2:
+                sub_agg_name = ''
+                for key in entry.keys():
+                    if key == 'doc_count' or key == 'key': continue
+                    sub_agg_name = key
+                    break
+                Searcher._parse(sub_agg_name, entry[sub_agg_name], output[agg_name][agg_key])
