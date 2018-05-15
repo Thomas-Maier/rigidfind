eb9033b3172e3225887538732b722546f3c4a2e6
Thomas Maier <tmaier@cern.ch>
2018-05-15 15:59:24 +0200

    update searcher parsing for percentiles aggregations

--- tools/searcher.py
+++ tools/searcher.py
@@ -20,18 +20,25 @@ class Searcher:
         with open(self._output, 'w') as out_file:
             json.dump(output, out_file)
 
+    ## parsing probably needs to be more aware of what is happening in the query
     @staticmethod
     def _parse(agg_name, agg_data, output):
         output[agg_name] = {}
-        for entry in agg_data['buckets']:
+        data_name = 'buckets'
+        if data_name not in agg_data:
+            data_name = 'values'
+        for entry in agg_data[data_name]:
             agg_key = entry['key']
             output[agg_name][agg_key] = {}
-            count = entry['doc_count']
+            count_name = 'doc_count'
+            if count_name not in entry:
+                count_name = 'value'
+            count = entry[count_name]
             output[agg_name][agg_key]['count'] = count
             if len(entry) > 2:
                 sub_agg_name = ''
                 for key in entry.keys():
-                    if key == 'doc_count' or key == 'key': continue
+                    if key == 'doc_count' or key == 'key' or key = 'value': continue
                     sub_agg_name = key
                     break
                 Searcher._parse(sub_agg_name, entry[sub_agg_name], output[agg_name][agg_key])
